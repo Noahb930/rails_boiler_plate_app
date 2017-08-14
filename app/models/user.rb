cd3e869a0d78b45220fc27e6443f4c9349a86d05
Noahb930 <noahb930@gmail.com>
2017-08-13 22:30:02 -0400

    Added validations for the user model
    First name must be 50 characters or less
    Last name must be 50 characters or less
    Username must be 50 characters or less, and unique
    Email must be 255 characters or less, unique, and in valid email format

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  validates :first_name,  presence: true, length: { maximum: 50 }
+  validates :last_name,  presence: true, length: { maximum: 50 }
+  validates :username,  presence: true, length: { maximum: 50 },
+                        uniqueness: { case_sensitive: false }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
 end
